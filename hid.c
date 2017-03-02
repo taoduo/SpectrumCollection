ec032feb4aa762abc2db7303f54f9b4e689ceb4e
Duo Tao <duo.tao@ligo.org>
2017-03-02 16:58:59 -0600

    test

--- hid.c
+++ hid.c
@@ -116,11 +116,11 @@ static int pthread_barrier_wait(pthread_barrier_t *barrier)
 extern "C" {
 #endif
 
-#ifdef DEBUG_PRINTF
+// #ifdef DEBUG_PRINTF
 #define LOG(...) fprintf(stderr, __VA_ARGS__)
-#else
-#define LOG(...) do {} while (0)
-#endif
+// #else
+// #define LOG(...) do {} while (0)
+// #endif
 
 #ifndef __FreeBSD__
 #define DETACH_KERNEL_DRIVER
