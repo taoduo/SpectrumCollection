9fab41274072621e589d97792dbf447f66cf972a
Duo Tao <duo.tao@ligo.org>
2017-04-11 14:25:51 -0500

    test

--- smpl1.c
+++ smpl1.c
@@ -47,7 +47,7 @@ bool smpl_FindTheHID()
 		if(devs != NULL) { //Just find the first spectrocope. Multiple scopes not currently supported
 				if(DevicePath != NULL)
 						free(DevicePath);
-				printf("vendor_id: %04x\n", devs->vendor_id);
+				printf("usage: %04x\n", devs->usage);
 				DevicePath = malloc((strlen(devs->path) + 1) * sizeof(char));
 				strcpy(DevicePath, devs->path);
 
